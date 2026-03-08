c520ed6ab3f8ac2d7e4d074ec1c11d19ad2c853e
matthiasF7 <m.frei@students.htl-leonding.ac.at>
2026-03-08 14:07:20 +0100

    Update Cesium access token and clean up viewer configuration

--- satelliten.js
+++ satelliten.js
@@ -1,12 +1,12 @@
 const satellites = [];
 let orbitLine = null;
 
-Cesium.Ion.defaultAccessToken = "DEIN_TOKEN";
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZWI5NzY3ZC04ZDhmLTQ3YTItOWI1NC0xODk2Y2IxOGExNWUiLCJpZCI6NDAwMjM2LCJpYXQiOjE3NzI5NzIzNDh9.0dEE051GmsG0amp5ptTAe2GfbhFg-QjgUpS_Ilmw8ls";
 
 const viewer = new Cesium.Viewer("cesiumContainer", {
-baseLayer: Cesium.ImageryLayer.fromWorldImagery(),
- terrainProvider: new Cesium.EllipsoidTerrainProvider(),
-  //baseLayerPicker: false,
+  baseLayer: Cesium.ImageryLayer.fromWorldImagery(),
+  terrainProvider: new Cesium.EllipsoidTerrainProvider(),
+  baseLayerPicker: false,
   geocoder: false,
   timeline: false,
   animation: false,
