4f33bdd61500ad584ec3289595e88df60cc1d5d3
matthiasF7 <m.frei@students.htl-leonding.ac.at>
2026-04-15 21:46:10 +0200

    Add defer attribute to script tags for improved loading performance in satellites.html; update error handling in loadSatellites function

--- satelliten.js
+++ satelliten.js
@@ -86,8 +86,6 @@ function updateVisibility() {
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZWI5NzY3ZC04ZDhmLTQ3YTItOWI1NC0xODk2Y2IxOGExNWUiLCJpZCI6NDAwMjM2LCJpYXQiOjE3NzI5NzIzNDh9.0dEE051GmsG0amp5ptTAe2GfbhFg-QjgUpS_Ilmw8ls";
 
 const viewer = new Cesium.Viewer("cesiumContainer", {
-  baseLayer: Cesium.ImageryLayer.fromWorldImagery(),
-  terrainProvider: new Cesium.EllipsoidTerrainProvider(),
   baseLayerPicker: false,
   geocoder: false,
   timeline: false,
@@ -205,7 +203,8 @@ function loadSatellites(url) {
       }
 
       updateVisibility();
-    });
+    })
+    .catch(e => console.error("Failed to load satellites", e));
 }
 
 // ---------------- Satellit erstellen ----------------
